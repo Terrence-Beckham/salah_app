0532f3eb09fc02d62b27a01d46414f9b2e440b7d
Terrence Beckham <terrencedbeckham@gmail.com>
2023-12-20 16:24:19 +0200

    Added and set up Salah Cubit
    Signed-off-by: Terrence Beckham <terrencedbeckham@gmail.com>

--- lib/salah/cubit/salah_cubit.dart
+++ lib/salah/cubit/salah_cubit.dart
@@ -1,8 +1,52 @@
 import 'package:bloc/bloc.dart';
-import 'package:flutter/material.dart';
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:salah_app/salah/models/salah.dart';
+import 'package:salah_repository/salah_repository.dart' hide Salah;
 
+part 'salah_cubit.g.dart';
 part 'salah_state.dart';
 
 class SalahCubit extends Cubit<SalahState> {
-  SalahCubit() : super(SalahInitial());
+  SalahCubit(this._salahRepository) : super(const SalahState());
+  final SalahRepository _salahRepository;
+
+  Future<void> fetchSalah(String city) async {
+    if (city == null || city.isEmpty) return;
+    emit(state.copyWith(status: SalahStatus.loading));
+
+    try {
+      final salah = Salah.fromRepository(await _salahRepository.getSalah(city));
+      emit(
+        state.copyWith(
+          status: SalahStatus.success,
+          salah: salah,
+        ),
+      );
+    } on Exception {
+      emit(state.copyWith(status: SalahStatus.failure));
+    }
+  }
+
+  Future<void> refreshWeather() async {
+    if (!state.status.isSuccess) return;
+    if (state.salah == Salah.empty) return;
+    try {
+      //Remove the hardcoded city
+      final salah = Salah.fromRepository(
+        await _salahRepository.getSalah('Chicago'),
+      );
+      emit(
+        state.copyWith(status: SalahStatus.success, salah: salah),
+      );
+    } on Exception {
+      emit(state);
+    }
+  }
+
+  @override
+  SalahState fromJson(Map<String, dynamic> json) => SalahState.fromJson(json);
+
+  @override
+  Map<String, dynamic> toJson(SalahState state) => state.toJson();
 }
--- lib/salah/cubit/salah_state.dart
+++ lib/salah/cubit/salah_state.dart
@@ -1,6 +1,36 @@
 part of 'salah_cubit.dart';
 
-@immutable
-sealed class SalahState {}
+enum SalahStatus { initial, loading, success, failure }
 
-final class SalahInitial extends SalahState {}
+extension SalahStatusX on SalahStatus {
+  bool get isInitial => this == SalahStatus.initial;
+  bool get isLoading => this == SalahStatus.loading;
+  bool get isSuccess => this == SalahStatus.success;
+  bool get isFailure => this == SalahStatus.failure;
+}
+
+@JsonSerializable()
+final class SalahState extends Equatable {
+  const SalahState({this.status = SalahStatus.initial, Salah? salah})
+      : salah = salah ?? Salah.empty;
+
+  factory SalahState.fromJson(Map<String, dynamic> json) =>
+      _$SalahStateFromJson(json);
+
+  final SalahStatus status;
+  final Salah salah;
+
+  SalahState copyWith({
+    SalahStatus? status,
+    Salah? salah,
+  }) {
+    return SalahState(
+      status: status ?? this.status,
+      salah: salah ?? this.salah,
+    );
+  }
+
+  Map<String, dynamic> toJson() => _$SalahStateToJson(this);
+  @override
+  List<Object?> get props => [status, salah];
+}
